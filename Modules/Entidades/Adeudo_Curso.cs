3e5f9e6b74aaa85fb3575d532135f8c1c2442fe1
Wilgrey Ravelo Cruz <167430214+Wilgrey2004@users.noreply.github.com>
2025-07-18 11:57:11 -0400

    Implementación de gestión de base de datos y UI
    Se han añadido configuraciones en `.gitattributes` y `.gitignore` para mejorar el manejo de archivos en el proyecto. Se ha modificado `App.config` para incluir una cadena de conexión a SQL Server.
    
    Se ha implementado la interfaz de usuario en `Form1.cs` utilizando `MaterialSkin` y `Bunifu`, con lógica de inicio de sesión. Se han realizado cambios en `Gestion.cs` para manejar la gestión de profesores y estudiantes, incluyendo métodos para cargar y editar datos.
    
    Se han añadido nuevos archivos de diseño y recursos para las clases de gestión, así como la implementación de `AppDbContext` y sus entidades. Se han creado formularios para editar información de estudiantes y cursos, y se han añadido nuevas imágenes y estilos para mejorar la apariencia visual de la aplicación.
    
    Además, se han realizado cambios significativos en varios archivos relacionados con la gestión de cursos y profesores, mejorando la funcionalidad y la presentación del sistema.

---
+++ Modules/Entidades/Adeudo_Curso.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace p_proyect.Modules.Entidades
+{
+        public class Adeudo_Curso
+        {
+                [Display(Name = "Codigo De la Deuda")]
+                [Key]
+                [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+                public long Id { get; set; }
+
+                [Display(Name = "Curso del adeudo")]
+                [ForeignKey("Curso")]
+                public long Id_Curso { get; set; }
+
+                public Cursos Curso { get; set; }
+
+                [Display(Name = "Codigo del Estudiante")]
+                [ForeignKey("Estudiante")]
+                public long Id_Estudiante { get; set; }
+                public Estudiante Estudiante { get; set; }
+
+                [Display(Name = "Total pagado")]
+                public decimal Total_Pagado { get; set; } = 0;
+
+                [Display(Name = "Restante a pagar")]
+                public decimal Restante_A_Pagar { get; set; } = 0;
+
+                [Display(Name = "Deudo total")]
+                public decimal Adeudo { get; set; } = 0;
+
+                public Adeudo_Curso() {
+
+                        this.Adeudo = Curso.Costo_Del_Curso;
+                        this.Restante_A_Pagar = Curso.Costo_Del_Curso - Total_Pagado;
+                }
+        }
+}
---
+++ Modules/Entidades/Empleado_Admin.cs
@@ -0,0 +1,36 @@
+﻿using p_proyect.Modules.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace p_proyect.Modules.Entidades
+{
+        public class Empleado_Admin
+        {
+                [Key]
+                [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+                public long Id { get; set; }
+
+                [Display(Name = "Nombre del Empleado")]
+                public string Nombre_del_Administrador { get; set; }
+
+                [Display(Name = "Apellido del Empleado")]
+                public string Apellido_del_Administrador { get; set; }
+
+                [Display(Name = "Numero de telefono")]
+                public string Numero_de_Telefono { get; set; }
+
+                [Display(Name = "Cotraseña")]
+                public string Pass { get; set; }
+
+                [Display(Name = "Contraseña confirmada")]
+                public string Pass_confirm { get; set; }
+
+                [Display(Name = "Estado del Empleado")]
+                public Estados_Generales Estado { get; set; } = Estados_Generales.Activo;
+
+                public override string ToString() {
+                        return $"{Nombre_del_Administrador} {Apellido_del_Administrador}";
+                }
+
+        }
+}
